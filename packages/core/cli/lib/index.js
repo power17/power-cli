a666639cbf6bb0ef2836bc2563dfc1f4f9bbaf1e
powr <1410837981@qq.com>
2023-05-12 18:45:54 +0800

    feat： 环境变量

--- packages/core/cli/lib/index.js
+++ packages/core/cli/lib/index.js
@@ -1,12 +1,17 @@
-module.exports = core
+const path = require('path')
 const pkg = require('./../package.json')
 const log = require('@power-cli/log')
 const constant = require('./const')
 const semver = require('semver')
 const colors = require('colors/safe')
+const userHome = require('user-home')
+const minimist = require('minimist')
+const dotenv = require('dotenv')
+let pathExists
 
+let argv
 
-function core(argv) {
+async function core(argv) {
   try{
     // 检测cli版本
     checkPkgVersion()
@@ -14,11 +19,68 @@ function core(argv) {
     checkNodeVersion()
     // 检查超级管理员, 如果是超级管理员就降级
     checkRoot()
-    console.log(process.env.SUDO_UID)
-  }catch(e){
-    log.error(e.message)
-  }
+    
+    pathExistsObj = await import('path-exists')
+    pathExists = pathExistsObj.pathExistsSync
+    // 检查用户主目录
+    checkUserHome(pathExists)
+   
+    // 检查用户输入
+    checkInputArgs()
+    // 检查环境变量
+    checkEnv()
 
+
+    }catch(e){
+      log.error(e.message)
+    }
+    
+
+}
+function checkEnv() {
+  const createDefaultConfig = () => {
+    const cliConfig = {
+      path: userHome
+    }
+    if(process.env.CLI_HOME) {
+      cliConfig['cliHome'] = path.join(userHome,process.env.CLI_HOME)
+    }else{
+      cliConfig['cliHome'] = path.join(userHome,constant.DEFAULT_CLI_HOME)
+    }
+    process.env.CLI_HOME_PATH = cliConfig['cliHome']
+    return cliConfig
+  }
+  // /usr/power/.env存在
+  const config = dotenv.config({path: path.resolve(userHome, '.env')})
+  const dotenvPath = path.resolve(userHome, '.env')
+  if(pathExists(dotenvPath)) {
+    dotenv.config({
+      path: dotenvPath
+    })
+   
+  }
+  createDefaultConfig()
+
+  log.info('环境变量路径：', process.env.CLI_HOME_PATH)
+  
+}
+
+function checkInputArgs() {
+  argv = minimist(process.argv.slice(2))
+  if(argv?.debug) {
+    process.env.LOCAL_LEVEL = 'verbose'
+  }else {
+    process.env.LOCAL_LEVEL = 'info'
+  }
+  log.level = process.env.LOCAL_LEVEL
+  log.verbose('debug: 开始调试')
+}
+//检查用户主目录
+function checkUserHome(pathExists) {
+  if(!userHome || !pathExists(userHome)) {
+    throw new Error(log.notice('当前登录用户主目录不存在'))
+  }
+  
 }
 function checkRoot() {
   const p = import('root-check')
@@ -42,3 +104,4 @@ function checkPkgVersion() {
   log.notice(pkg.version)
 
 }
+module.exports = core
